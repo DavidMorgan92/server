4b4442eeea822a6ec1dfe6658ca1ceb4dc16a4c6
David Morgan <david.morgan@europe.com>
2022-10-30 17:37:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Server
+
+A template for a REST server with authorisation facilitated by JWT access and refresh tokens.
+
+## Configuration
+
+| Variable      | Description                                                      |
+| ------------- | ---------------------------------------------------------------- |
+| PORT          | Port number that server listens on                               |
+| CLIENT_ORIGIN | Origin of client requests for Access-Control-Allow-Origin header |
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+
+// Configure environment variables
+dotenv.config();
+
+// Create Express app
+const app = express();
+
+// Use Helmet
+app.use(helmet());
+
+// Use Morgan logger (disable if running tests)
+if (process.env.NODE_ENV !== 'test') {
+	app.use(morgan('dev'));
+}
+
+// Configure CORS
+app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
+
+// Home route
+app.get('/', (_, res) => {
+	res.send('Hello, World!');
+});
+
+// Start server
+const port = Number(process.env.PORT);
+app.listen(port, () => console.log(`Server listening on port ${port}`));
